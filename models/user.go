31bedc26636ba38b929f2662a78b2095f3648cf9
Jo <zhiliao0807@yahoo.com>
2023-12-21 16:53:36 +0800

    User -- Register & Save

--- models/user.go
+++ models/user.go
@@ -1,86 +1,55 @@
 package models
 
 import (
-	"errors"
-	"strconv"
+	"github.com/astaxie/beego/orm"
+	"log"
 	"time"
 )
 
-var (
-	UserList map[string]*User
-)
+type User struct {
+	Id       int
+	Name     string
+	Password string
+	Status   int
+	AddTime  int64
+	Mobile   string
+	Avatar   string
+}
+
+type UserInfo struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	AddTime int64  `json:"addTime"`
+	Avatar  string `json:"avatar"`
+}
 
 func init() {
-	UserList = make(map[string]*User)
-	u := User{"user_11111", "astaxie", "11111", Profile{"male", 20, "Singapore", "astaxie@gmail.com"}}
-	UserList["user_11111"] = &u
+	orm.RegisterModel(new(User))
 }
 
-type User struct {
-	Id       string
-	Username string
-	Password string
-	Profile  Profile
-}
-
-type Profile struct {
-	Gender  string
-	Age     int
-	Address string
-	Email   string
-}
-
-func AddUser(u User) string {
-	u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	UserList[u.Id] = &u
-	return u.Id
-}
-
-func GetUser(uid string) (u *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		return u, nil
+func IsUserMobile(mobile string) bool {
+	o := orm.NewOrm()
+	user := User{Mobile: mobile}
+	err := o.Read(&user, "Mobile")
+	if err == orm.ErrNoRows {
+		log.Printf("IsUserMobile: no row found")
+		return false
+	} else if err == orm.ErrMissPK {
+		log.Printf("IsUserMobile: missed pk value")
+		return false
 	}
-	return nil, errors.New("User not exists")
+	return true
 }
 
-func GetAllUsers() map[string]*User {
-	return UserList
-}
-
-func UpdateUser(uid string, uu *User) (a *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		if uu.Username != "" {
-			u.Username = uu.Username
-		}
-		if uu.Password != "" {
-			u.Password = uu.Password
-		}
-		if uu.Profile.Age != 0 {
-			u.Profile.Age = uu.Profile.Age
-		}
-		if uu.Profile.Address != "" {
-			u.Profile.Address = uu.Profile.Address
-		}
-		if uu.Profile.Gender != "" {
-			u.Profile.Gender = uu.Profile.Gender
-		}
-		if uu.Profile.Email != "" {
-			u.Profile.Email = uu.Profile.Email
-		}
-		return u, nil
+func UserSave(mobile string, password string) error {
+	o := orm.NewOrm()
+	user := User{
+		Name:     "",
+		Password: password,
+		Status:   1,
+		AddTime:  time.Now().Unix(),
+		Mobile:   mobile,
 	}
-	return nil, errors.New("User Not Exist")
-}
-
-func Login(username, password string) bool {
-	for _, u := range UserList {
-		if u.Username == username && u.Password == password {
-			return true
-		}
-	}
-	return false
-}
-
-func DeleteUser(uid string) {
-	delete(UserList, uid)
+	_, err := o.Insert(&user)
+	return err
 }
